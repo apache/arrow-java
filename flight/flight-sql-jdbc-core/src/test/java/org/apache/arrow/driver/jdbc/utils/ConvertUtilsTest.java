698469bb3ff940f0a52413916b323582f2be2d0a
chenweiguo.vc <chenweiguo.vc@bytedance.com>
2025-11-19 21:32:22 +0800

    fix test

--- flight/flight-sql-jdbc-core/src/test/java/org/apache/arrow/driver/jdbc/utils/ConvertUtilsTest.java
+++ flight/flight-sql-jdbc-core/src/test/java/org/apache/arrow/driver/jdbc/utils/ConvertUtilsTest.java
@@ -61,6 +61,19 @@ public class ConvertUtilsTest {
                 "col1",
                 new FieldType(
                     true,
+                    ArrowType.Utf8.INSTANCE,
+                    null,
+                    new FlightSqlColumnMetadata.Builder()
+                        .catalogName("catalog1")
+                        .schemaName("schema1")
+                        .tableName("table1")
+                        .build()
+                        .getMetadataMap()),
+                null),
+            new Field(
+                "col2",
+                new FieldType(
+                    true,
                     ArrowType.Utf8View.INSTANCE,
                     null,
                     new FlightSqlColumnMetadata.Builder()
@@ -79,6 +92,18 @@ public class ConvertUtilsTest {
                     .setSchemaName("schema1")
                     .setTableName("table1")
                     .setColumnName("col1")
+                    .setType(
+                        Common.AvaticaType.newBuilder()
+                            .setId(SqlTypes.getSqlTypeIdFromArrowType(ArrowType.Utf8.INSTANCE))
+                            .setName(SqlTypes.getSqlTypeNameFromArrowType(ArrowType.Utf8.INSTANCE))
+                            .build())
+                    .build()),
+            ColumnMetaData.fromProto(
+                Common.ColumnMetaData.newBuilder()
+                    .setCatalogName("catalog1")
+                    .setSchemaName("schema1")
+                    .setTableName("table1")
+                    .setColumnName("col2")
                     .setType(
                         Common.AvaticaType.newBuilder()
                             .setId(SqlTypes.getSqlTypeIdFromArrowType(ArrowType.Utf8View.INSTANCE))
